d4b3001f7b2246b34b0f63d915cc0b46873cc647
Hemang <hemangsharma1001@gmail.com>
2024-02-10 16:14:21 +0530

    completed

---
+++ script.js
@@ -0,0 +1,31 @@
+const passwordBox = document.getElementById("password");
+console.log(passwordBox) ;
+const length = 10 ;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; 
+const lowerCase = "abcdefghijklmnopqrstuvwxyz" ;
+const number = "0123456789" ; 
+const symbols = "~!@#$%^&*_";
+
+const allChars = upperCase + lowerCase + number + symbols; 
+function createPassword(){
+    let password ="";
+
+    password+= upperCase[Math.floor(Math.random() * upperCase.length)]; 
+    password+= lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password+= number[Math.floor(Math.random() * number.length)];
+    password+= symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length > password.length)
+    {
+        password+= allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
